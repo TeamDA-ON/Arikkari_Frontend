de093beac6c6d8da1b7ca57c9e8b5b220c20537c
kwonkangbin <kbk282655@gmail.com>
2023-09-05 23:47:38 +0800

    ADD : selectBox 컴포넌트

--- lib/pages/quizSelection/quizSelection.dart
+++ lib/pages/quizSelection/quizSelection.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/widgets/pages/quizSelection/selectBox.dart';
 
 class quizSelection extends StatelessWidget {
   const quizSelection({super.key});
@@ -8,45 +9,19 @@ class quizSelection extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
       child: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 1,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: Colors.black,
-                width: 5,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
-              child: Column(
-                children: const [
-                  Text(
-                    '맞춤법 퀴즈',
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontFamily: "Pretendard",
-                      fontWeight: FontWeight.w700,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(
-                    '문장을 듣고 빈칸을 채워 맞춤법을 공부해요.',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: "Pretendard",
-                      fontWeight: FontWeight.w700,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+        children: const [
+          selectBox(
+            title: '맞춤법 퀴즈',
+            subTitle: '문장을 듣고 빈칸을 채워 맞춤법을 공부해요.',
+            textColor: Color(0xFF5B5DC9),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          selectBox(
+            title: '어휘 퀴즈',
+            subTitle: '문장에 알맞은 단어를 찾아 어휘를 키워요.',
+            textColor: Color(0xFF92B4EC),
           ),
         ],
       ),
