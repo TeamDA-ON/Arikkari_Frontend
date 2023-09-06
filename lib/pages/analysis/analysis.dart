ec7cb0601882e09a9a45a4577bbd9f1e4c142f1b
kwonkangbin <kbk282655@gmail.com>
2023-09-06 23:31:13 +0800

    ADD : 레거시 코드

---
+++ lib/pages/analysis/analysis.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/widgets/appbar.dart';
+
+class Analysis extends StatefulWidget {
+  const Analysis({Key? key}) : super(key: key);
+
+  @override
+  State<Analysis> createState() => _AnalysisState();
+}
+
+class _AnalysisState extends State<Analysis> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: appBar(),
+        backgroundColor: const Color.fromRGBO(120, 120, 120, 1),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 85, 20, 40),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            elevation: 10,
+            child: SizedBox(
+              width: double.maxFinite,
+              height: 380,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 85, 20, 40),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'assets/img/analysis.png',
+                        width: 120,
+                        height: 120,
+                      ),
+                      const Text('준비중'),
+                      const Text('아직 제공되지 않는 기능입니다.'),
+                      const Text('잠시만 기다려 주세요.')
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/pages/home/home.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/pages/choseQuiz/chosequiz.dart';
+import 'package:get/get.dart';
+import 'package:flutter_project/widgets/appbar.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: QuizStart(),
+    );
+  }
+}
+
+class QuizStart extends StatefulWidget {
+  const QuizStart({super.key});
+
+  @override
+  State<QuizStart> createState() => _QuizStartState();
+}
+
+class _QuizStartState extends State<QuizStart> {
+  String todayWorld = 'baek';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Image.asset(
+              'assets/img/homeLogo.png',
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            const Text(
+              '퀴즈 풀러가기',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.to(ChoseQuiz());
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+              ),
+              child: Image.asset(
+                'assets/img/start.png',
+                width: 99,
+                height: 99,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.07,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              margin: const EdgeInsets.only(bottom: 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    todayWorld,
+                    style: const TextStyle(
+                        fontSize: 38,
+                        fontWeight: FontWeight.w600,
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  height: MediaQuery.of(context).size.height * 0.2,
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    color: Colors.yellow,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
