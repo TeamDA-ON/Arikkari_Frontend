3548187874ca8c27223ed9a53c29302078a663ce
kwonkangbin <kbk282655@gmail.com>
2023-09-11 19:57:46 +0900

    FIX : selectBox 에러 고치기

--- lib/pages/quizSelection/quizSelection.dart
+++ lib/pages/quizSelection/quizSelection.dart
@@ -16,7 +16,7 @@ class quizSelection extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             selectBox(
-              onTap: QuizController(),
+              onTaps: QuizController(),
               title: '맞춤법 퀴즈',
               subTitle: '문장을 듣고 빈칸을 채워 맞춤법을 공부해요.',
               textColor: const Color(0xFF5B5DC9),
@@ -25,7 +25,7 @@ class quizSelection extends StatelessWidget {
               height: 40,
             ),
             selectBox(
-              onTap: QuizController(),
+              onTaps: QuizController(),
               title: '어휘 퀴즈',
               subTitle: '문장에 알맞은 단어를 찾아 어휘를 키워요.',
               textColor: const Color(0xFF92B4EC),
