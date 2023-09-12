c70474acbc866085d2f23a579fe145a36d37c067
kwonkangbin <kbk282655@gmail.com>
2023-09-12 16:11:03 +0900

    DESIGN : 디자인시스템 색깔

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._(); ////instance 할 수 없음
+  static const Color blueF2 = Color(0xFF7173C9);
+  static const Color blueF3 = Color(0xFF75A3EF);
+  static const Color blueF4 = Color(0xFF92B4EC);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGrayF1 = Color(0xFFE7E7E7);
+  static const Color darkGrayF1 = Color(0xFF404040);
+  static const Color darkGrayF2 = Color(0xFF7A7A7A);
+}
