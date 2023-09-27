58a9ddba9b4fa68bbc103cc39fa3a0c5dd5773ee
kwonkangbin <kbk282655@gmail.com>
2023-09-28 01:07:44 +0900

    ADD : API 연동 정답 여부 판정

--- lib/ui/screen/spelling-quiz/spelling-quiz.dart
+++ lib/ui/screen/spelling-quiz/spelling-quiz.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/state/quiz/quiz_getx.dart';
 import 'package:flutter_project/ui/widgets/pages/spelling-quiz/spelling-quiz.dart';
+import 'package:flutter_project/utilities/logger.dart';
 import 'package:get/get.dart';
 
 int? id;
@@ -10,27 +11,34 @@ String? answer;
 String? problem1;
 String? problem2;
 String? commentary;
+late final response;
 
-class Quiz extends StatelessWidget {
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
-  Future<void> getData() async {
-    var dio = Dio();
-    try {
-      final response = await dio.get(
-          'https://port-0-arikkari-backend-mvp-2rrqq2blmy418s6.sel5.cloudtype.app/api/saq/get');
-      difficulty = response.data[0]['difficulty'];
-      answer = response.data[0]['answer'];
-      problem1 = response.data[0]['problem1'];
-      problem2 = response.data[0]['problem2'];
-      commentary = response.data[0]['commentary'];
-    } catch (error) {
-      print('에러 :: $error');
-    }
-  }
+  @override
+  State<Quiz> createState() => _QuizState();
+}
 
+class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
+    Future<void> getData() async {
+      var dio = Dio();
+      try {
+        response = await dio.get(
+            'https://port-0-arikkari-backend-mvp-2rrqq2blmy418s6.sel5.cloudtype.app/api/saq/get');
+        // difficulty = response.data[0]['difficulty'];
+        // answer = response.data[0]['answer'];
+        // problem1 = response.data[0]['problem1'];
+        // problem2 = response.data[0]['problem2'];
+        // commentary = response.data[0]['commentary'];
+        logger.d(response.data);
+      } catch (error) {
+        print('에러 :: $error');
+      }
+    }
+
     Get.put(QuizGetx()); //컨트롤러에 종속성 주입
     return GetBuilder<QuizGetx>(
       builder: (x) {
@@ -56,23 +64,35 @@ class Quiz extends StatelessWidget {
                         SizedBox(
                           height: Get.height * 0.1,
                         ),
-                        spelling_QuizContainer(
-                          problem1: problem1,
-                          problem2: problem2,
-                          difficulty: difficulty,
-                          answer: answer,
-                          ttsTap: () {
-                            x.usingTts(
-                              problem1: problem1,
-                              problem2: problem2,
-                              answer: answer,
-                            );
-                          },
-                          editController: x.textEditController,
-                          submit: () {
-                            x.checkAnswer();
-                          },
-                          commentary: '',
+                        Obx(
+                          () => spelling_QuizContainer(
+                            problem1: response.data[x.progress.value]
+                                ['problem1'],
+                            problem2: response.data[x.progress.value]
+                                ['problem2'],
+                            difficulty: response.data[x.progress.value]
+                                ['difficulty'],
+                            answer: response.data[x.progress.value]['answer'],
+                            ttsTap: () {
+                              x.usingTts(
+                                problem1: response.data[x.progress.value]
+                                    ['problem1'],
+                                problem2: response.data[x.progress.value]
+                                    ['problem2'],
+                                answer: response.data[x.progress.value]
+                                    ['answer'],
+                              );
+                            },
+                            editController: x.textEditController,
+                            submit: () {
+                              x.checkAnswer(
+                                answer: response.data[x.progress.value]
+                                    ['answer'],
+                              );
+                            },
+                            commentary: response.data[x.progress.value]
+                                ['commentary'],
+                          ),
                         ),
                         SizedBox(
                           height: Get.height * 0.1,
