1a18440eea4e29b51ed46da80fe1113b7834328d
kwonkangbin <kbk282655@gmail.com>
2023-10-08 13:56:21 +0900

    CHORE: 테스트버튼 삭제

--- lib/ui/widgets/pages/spelling-quiz/spelling-quiz.dart
+++ lib/ui/widgets/pages/spelling-quiz/spelling-quiz.dart
@@ -151,36 +151,6 @@ Stack spelling_QuizContainer({
                 ],
               ),
             ),
-            //test button
-            GestureDetector(
-              onTap: checkAnswer,
-              child: Container(
-                width: 80,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: const Color(0xFFf4f4f4),
-                  border:
-                      Border.all(color: const Color(0xFFD9D9D9), width: 3.0),
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      "확인",
-                      style: TextStyle(
-                        letterSpacing: 5,
-                        color: Color(
-                          0xFF7A7A7A,
-                        ),
-                        fontSize: 22.0,
-                        fontFamily: "Pretendard",
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
           ],
         ),
       ),
