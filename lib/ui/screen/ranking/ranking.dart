d06c65ba7df220d546bbb29706b0ee2d1a3bd696
kwonkangbin <kbk282655@gmail.com>
2023-10-15 21:34:04 +0900

    ADD : playerRanking 컴포넌트화

--- lib/ui/screen/ranking/ranking.dart
+++ lib/ui/screen/ranking/ranking.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/ui/widgets/constants/appbar.dart';
-import 'package:get/get.dart';
+import 'package:flutter_project/ui/widgets/pages/Ranking/playerRanking.dart';
 
 class Ranking extends StatelessWidget {
   const Ranking({super.key});
@@ -13,66 +13,14 @@ class Ranking extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(44, 24, 44, 0),
         child: Column(
           children: [
-            Container(
-              width: Get.width,
-              height: 62,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(
-                  color: const Color(0xFFFEE500),
-                  width: 1,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(22, 18, 22, 18),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      '1',
-                      style: TextStyle(
-                        color: Color(0xFFFEE500),
-                        fontSize: 22,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w600,
-                        height: 0,
-                      ),
-                    ),
-                    Text(
-                      '정용명',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w700,
-                        height: 0,
-                      ),
-                    ),
-                    Text(
-                      '부산소마고',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w700,
-                        height: 0,
-                      ),
-                    ),
-                    Text(
-                      '420',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w700,
-                        height: 0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
+            playerRanking(
+                belong: '무소속', name: '김강민', ranking: '1', score: '400'),
+            playerRanking(
+                belong: '무소속', name: '박강인', ranking: '2', score: '300'),
+            playerRanking(
+                belong: '무소속', name: '권강빈', ranking: '3', score: '200'),
+            playerRanking(
+                belong: '무소속', name: '우수지', ranking: '4', score: '100')
           ],
         ),
       ),
