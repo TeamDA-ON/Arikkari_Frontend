9bd99a4dc4c04d741ad126fab5c2a7430392b8da
kwonkangbin <kbk282655@gmail.com>
2023-10-17 23:45:02 +0900

    FIX: 링크 오류

--- lib/ui/screen/login/webview.dart
+++ lib/ui/screen/login/webview.dart
@@ -18,7 +18,7 @@ class _WebviewState extends State<Webview> {
   @override
   Widget build(BuildContext context) {
     const authUrl =
-        'https://accounts.google.com/o/oauth2/v2/auth?client_id=415045963497-6a4h12l94ovshlq70jmfjlh3susd8g91.apps.googleusercontent.com&redirect_uri=https://port-0-arikkari-backend-euegqv2blnrdvf3e.sel5.cloudtype.app/login/oauth2/code/google&response_type=code&scope=email';
+        'https://accounts.google.com/o/oauth2/v2/auth?client_id=415045963497-6a4h12l94ovshlq70jmfjlh3susd8g91.apps.googleusercontent.com&redirect_uri=https://port-0-arikkari-backend-euegqv2blnrdvf3e.sel5.cloudtype.app/login/oauth2/code/google&response_type=code&scope=email profile';
     return MaterialApp(
       home: Scaffold(
         body: WebView(
@@ -38,16 +38,13 @@ class _WebviewState extends State<Webview> {
               var authuser = urlParameters['authuser'];
               var prompt = urlParameters['prompt'];
 
-              logger.d(code);
-              logger.d(request.url);
-
               if (code != null &&
                   scope != null &&
                   authuser != null &&
                   prompt != null) {
                 // Create a new URI with the query parameters.
                 Uri uri = Uri.parse(
-                    "https://port-0-arikkari-backend-euegqv2binrdvf3e.sel5.cloudtype.app/api/auth/signup?code=$code&scope=$scope&authuser=$authuser&prompt=$prompt");
+                    "https://port-0-arikkari-backend-euegqv2blnrdvf3e.sel5.cloudtype.app/api/auth/signup?code=$code&scope=$scope&authuser=$authuser&prompt=$prompt");
                 logger.d(uri);
 
                 var response = await http.post(uri);
