9229f1b03c234f7229923212fd2df0d2664b9ffa
useonguk <ysu0219@gmail.com>
2023-11-04 22:28:36 +0900

    CHORE

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/ui/screen/login/login.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -8,8 +9,8 @@ void main() {
       theme: ThemeData(
         fontFamily: 'Pretendard',
       ),
-      home: const Quiz(),
-      // home: const Login(),
+      // home: const Spelling(),
+      home: const Login(),
     ),
   );
 }
