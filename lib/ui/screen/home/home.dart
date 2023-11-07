f57df129809a27129fc4e6ba3e745889b935e26f
useonguk <ysu0219@gmail.com>
2023-11-07 16:27:46 +0900

    FIX: overflow

--- lib/ui/screen/home/home.dart
+++ lib/ui/screen/home/home.dart
@@ -71,7 +71,7 @@ class Home extends StatelessWidget {
                         children: [
                           Container(
                             width: MediaQuery.of(context).size.width * 0.8,
-                            height: MediaQuery.of(context).size.height * 0.35,
+                            height: MediaQuery.of(context).size.height * 0.3,
                             decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(30),
